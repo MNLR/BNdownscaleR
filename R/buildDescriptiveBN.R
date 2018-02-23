801fda486ab76dc276194bda817f67bf7b388d72
MNLR <legasam@unican.es>
2018-02-23 19:31:15 +0100

    - Implemented buildDescriptiveBN() and its auxiliary function prepare_Dataset_forDescriptiveBN(), -- Builds a Bayesian Network directly from stations data, i.e. as directly downloaded using loadStationData() from SantanderMetGroup loadeR. -- prepare_Dataset_forDescriptiveBN() is an internal function for the former. The name is self-descriptive.

---
+++ R/buildDescriptiveBN.R
@@ -0,0 +1,21 @@
+#'
+#' @export
+
+buildDescriptiveBN <- function(y, structure.learning.algorithm =  "hc",
+                               structure.learning.args.list = list(),
+                               compile.junction = TRUE,
+                               param.learning.method = "bayes",
+                               output.marginals = TRUE,
+                               parallelize = FALSE, cluster.type = "FORK", n.cores = NULL) {
+  py <- prepare_Dataset_forDescriptiveBN(y)
+  descbn <- build.downscalingBN(py, structure.learning.algorithm = structure.learning.algorithm,
+                      structure.learning.args.list = structure.learning.args.list,
+                      param.learning.method = param.learning.method, forbid.GG = FALSE, forbid.DD = FALSE,
+                      compile.junction = compile.junction,
+                      output.marginals = output.marginals,
+                      parallelize = parallelize, cluster.type = cluster.type, n.cores = n.cores)
+
+  # Compute loglikelihood...
+
+  return(descbn)
+}
