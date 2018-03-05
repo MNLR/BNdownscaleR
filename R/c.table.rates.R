2c6437c274569fcd70dfb86d2533c5c097dc33aa
MNLR <legasam@unican.es>
2018-03-05 15:05:17 +0100

    - Fixed a bug in setEvidence() that made it crash.
    - c.table.rates() returns now "all" by default.

--- R/c.table.rates.R
+++ R/c.table.rates.R
@@ -1,14 +1,14 @@
-c.table.rates <- function(c.table, value ){
+c.table.rates <- function(c.table, value = "all" ){
   switch(value,
          TPR = { return( c.table[2,2]/(c.table[2,2]+c.table[1,2]) ) },
          FPR = { return( c.table[2,1]/(c.table[2,1]+c.table[1,1]) ) },
          TNR = { return( c.table[1,1]/(c.table[1,1]+c.table[2,1]) ) },
-         FNR = { return( c.table[1,2]/(c.table[1,2]+c.table[2,2]) ) }, 
+         FNR = { return( c.table[1,2]/(c.table[1,2]+c.table[2,2]) ) },
          MSC = { return( (c.table[2,1] + c.table[1,2])/sum(c.table) ) },
          all = { return( list(TPR = c.table[2,2]/(c.table[2,2]+c.table[1,2]),
                               FPR = c.table[2,1]/(c.table[2,1]+c.table[1,1]),
                               TNR = c.table[1,1]/(c.table[1,1]+c.table[2,1]),
-                              FNR = c.table[1,2]/(c.table[1,2]+c.table[2,2]), 
+                              FNR = c.table[1,2]/(c.table[1,2]+c.table[2,2]),
                               MSC = (c.table[2,1] + c.table[1,2])/sum(c.table) )
          )
          }
--- R/queryJunction.R
+++ R/queryJunction.R
@@ -1,6 +1,6 @@
 #' @export
 
 queryJunction <- function( ev, predictors, junction, predictands) {
-  evid <- setEvidence(junction, predictors, as.character(categorized)) # Evidence must be provided as character
+  evid <- setEvidence(junction, predictors, as.character(ev)) # Evidence must be provided as character
   return( querygrain(evid, nodes = predictands, type = "marginal") )
 }
