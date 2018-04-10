49feb718629017fc20c8398db7a37479cde940c8
MNLR <legasam@unican.es>
2018-04-10 18:26:19 +0200

    - Added files -- IBN.R, which contains a reproducible example over the the north of the Iberian Peninsula. -- GER.R, which contains a reproducible example for Germany. --- Both are ignored in .Rbuildignore
    - Forgotten check.

---
+++ GER.R
@@ -0,0 +1,87 @@
+####################################################
+library(downscaleR.BN)                             #
+####################################################
+####################################################
+####################################################
+####################################################
+
+# library(loadeR)
+# loginUDG('Mitor', '450cacahuetes')
+# dataInventory("http://meteo.unican.es/tds5/dodsC/interim/daily/interim20_daily.ncml")
+# xx <- loadGridData(dataset = "http://meteo.unican.es/tds5/dodsC/interim/daily/interim20_daily.ncml",
+#                    var = "TP", lonLim = c(4,16), latLim = c(46,58), years = 1979:2008
+#                    )
+# yy <- loadStationData(dataset = "data/VALUE_ECA_53_Germany_spatial_v1.zip",
+#                      var = "precip", years = 1979:2008
+#                      )
+# xx$Data <- xx$Data*1000
+# xx$Data[xx$Data >= 1] <- 1
+# xx$Data[xx$Data < 1] <- 0
+# yy$Data[yy$Data >= 1] <- 1
+# yy$Data[yy$Data < 1] <- 0
+
+##
+##    EQUALS
+##
+load("data/xx.RData")
+load("data/yy.RData")
+##
+
+gridGER <- prepare_predictors(x = xx, y = yy)
+dataGER <- prepare_predictors.forBN(grid = gridGER)
+
+descbn <- buildDescriptiveBN(yy, structure.learning.algorithm = "hc")
+plotDBN(descbn, dev = TRUE, nodes = 1)
+
+dbnGER <- build.downscalingBN(dataGER,
+                             structure.learning.algorithm = "hc", #structure.learning.args.list = list(distance = 2),
+                             param.learning.method = "bayes"
+                             )
+
+plotDBN(dbnGER, dev = TRUE)
+# exp.name <- "dbnGER.pdf"
+# dev.print(pdf, file = paste0("exampleplots/", exp.name), width = 15, height = 15)
+
+txx <- getTemporalIntersection(obs = filterNA(yy), prd = filterNA(xx), which.return = "prd")
+tyy <- getTemporalIntersection(obs = filterNA(yy), prd = filterNA(xx), which.return = "obs")
+testGER <- prepare_newdata(newdata = txx,
+                        predictor = gridGER)
+
+###
+### Prediction
+###
+pyy <- downscaleBN(dbnGER, x = testGER,
+                  output = "probabilities", prediction.type = "exact",
+                  parallelize = TRUE, n.cores = 3 , cluster.type = "FORK")
+###
+### Simulation:
+###
+syy <- downscaleBN(dbnGER, x = testGER,
+                   output = "event", prediction.type = "simulation",
+                   parallelize = TRUE, n.cores = 3 , cluster.type = "FORK")
+
+
+aucGER <- aucDBN(downscaled = pyy$member_1, realData = tyy$Data)
+c(max(aucGER), min(aucGER), mean(aucGER))
+
+cthreshold <- findClimatologyThreshold(pyy$member_1, dbnGER$marginals[2 , ])
+pyye <- convertEvent( pyy$member_1, threshold.vector = cthreshold)
+table(pyye)
+table(tyy$Data)
+cTableRates(cTable(predicted = pyye, real = tyy$Data ))
+
+phiGGER <- measureMatrix(dataGER$data[ , dataGER$x.names])
+dGGER <- measureMatrix(list(Data = dataGER$data[ , dataGER$x.names],
+                            xyCoords = t(dataGER$positions[ , dataGER$x.names] )),
+                       measure = "distance"
+                      )
+dev.new()
+distanceBias(real = tyy, prediction = pyye)
+points(dGGER, phiGGER, col = "green")
+
+dev.new()
+distanceBias(real = tyy,
+             prediction = convertEvent( pyy$member_1,
+                                        threshold.vector = rep(0.5, length(dataGER$y.names)))
+             )
+points(dGGER, phiGGER, col = "green")
---
+++ IBN.R
@@ -0,0 +1,100 @@
+####################################################
+library(downscaleR.BN)                             #
+####################################################
+####################################################
+####################################################
+####################################################
+
+#####################################################
+# library(loadeR)                                   #
+# loginUDG('Mitor', '450cacahuetes')                #
+#####################################################
+
+### datasets
+#y <- loadStationData('/home/w/Dropbox/R/BNdownscale/data/VALUE/AEMET', var = "precip")
+#di <-dataInventory('http://meteo.unican.es/tds5/dodsC/interim/interim075.ncml')
+#x <- loadGridData('http://meteo.unican.es/tds5/dodsC/interim/interim075.ncml', var = 'tp',
+#                   lonLim = c(-7,-1), latLim = c(43,43.75), time = "DD" ,aggr.d = "mean")
+# Traslado de grid
+# x <- interpGrid(x, new.coordinates = list(x = x$xyCoords$x, y = x$xyCoords$y + 0.25), method = "nearest")
+
+#######
+#######
+#x <- getTemporalIntersection(obs = y, prd = x, which.return = "prd")
+#y <- getTemporalIntersection(obs = y, prd = x, which.return = "obs")
+
+##
+##    EQUALS
+##
+load("data/interim075_43_43.75_-7_-1_DDmean.RData") # loads x
+load("data/AEMET_precip.RData") # loads y
+##
+
+grid <- prepare_predictors(x = x,y = y)
+data <- prepare_predictors.forBN(grid = grid, rm.na = TRUE, rm.na.mode = "observations")
+#Ddata <- prepareDataDynamicBN(data, 3)
+
+descbn <- buildDescriptiveBN(y,
+                             structure.learning.algorithm = "hc",
+                             structure.learning.args.list = list(distance = 1)
+                             )
+plotDBN(descbn, dev = TRUE, nodes = 1)
+
+dbn <- build.downscalingBN(data)
+
+dbncomplex <- build.downscalingBN(data,
+                                  structure.learning.algorithm = "mmhc",
+                                  structure.learning.args.list = list(distance = 2),
+                                  param.learning.method = "bayes",
+                                  forbid.GG = TRUE, forbid.DD = FALSE,
+                                  dynamic = TRUE, epochs = 2, remove.past.G = TRUE,
+                                  keep.dynamic.distance = TRUE,
+                                  structure.learning.steps = c("local", "global", "past"),
+                                  fix.intermediate = TRUE,
+                                  structure.learning.algorithm2 = "tabu",
+                                  structure.learning.args.list2 = list( distance = 2 ),
+                                  structure.learning.algorithm3 = "hc",
+                                  structure.learning.args.list3 = list( distance = 0.1 ),
+                                  return.intermediate = TRUE,
+                                  forbid.backwards = FALSE, forbid.past.dynamic.GD = TRUE,
+                                  forbid.dynamic.GG = TRUE, forbid.past.DD = TRUE,
+                                  compile.junction = FALSE,
+                                  parallelize = TRUE, n.cores = NULL, cluster.type = "FORK"
+                                  )
+
+plotDBN(dbn, dev = TRUE)
+plotDBN(dbncomplex$intermediateDBN1, dev = TRUE, nodes = 1)
+plotDBN(dbncomplex)
+# exp.name <- "phiCG.pdf"
+# dev.print(pdf, file = paste0("exampleplots/", exp.name), width = 15, height = 15/2)
+
+tx <- getTemporalIntersection(obs = filterNA(y), prd = filterNA(x), which.return = "prd")
+ty <- getTemporalIntersection(obs = filterNA(y), prd = filterNA(x), which.return = "obs")
+test <- prepare_newdata(newdata = tx,
+                        predictor = grid)
+
+###
+### Prediction
+###
+py <- downscaleBN(dbn, x = test, output = "probabilities", prediction.type = "exact",
+                  parallelize = FALSE, n.cores = 3 , cluster.type = "FORK")
+
+###
+### Simulation
+###
+sy <- downscaleBN(dbn, x = test,
+                        prediction.type = "simulation",
+                        parallelize = TRUE, n.cores = 3 , cluster.type = "FORK")
+
+
+dbnaucS <- aucDBN(downscaled = py$member_1, realData = ty$Data, plot.curves = TRUE,
+                  return.YI = TRUE)
+pye <- convertEvent( py$member_1, threshold.vector = 0.5)
+
+cTableRates(cTable(predicted = pye, real = ty$Data))
+cTable(predicted = pye, real = ty$Data)
+distanceBias(real = ty, prediction = pye )
+
+aucDBNvs(realData = ty$Data, downscaled = py$member_1, downscaled2 = sy$member_1,
+         is.event2 = TRUE)
+
